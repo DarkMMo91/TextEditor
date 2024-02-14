4d3e64c9b53c7aee963ddef4dc8f96f73f475dd9
Mark <soldano9carlo@gmail.com>
2024-02-14 22:24:36 +0100

    Data: 14/02/2024

--- Main/text.py
+++ Main/text.py
@@ -1,39 +1,92 @@
-from tkinter import * 
-from tkinter import ttk
+import random
+from tkinter import *
+from tkinter import filedialog
 
 #MainRoot
 root = Tk()
 
 #Constante
 
-WHITE = "FFFFFF"
+White = "white"
+Black = "black"
+
 #Variabili
 
-root.geometry("820x820")
+color = White
+
+#Variabili per la finestra
+
+root.geometry("1280x720")
 root.title("Teditor")
-root.iconbitmap("C:/Users/39339/Desktop/Python/TextEditor/Main/Teditor_Icon.ico")
+root.iconbitmap("Main/Teditor_Icon.ico")
 
-#Funzioni
+#Funzioni della MainBar
 
-def FunzioneTest():
-    print(InputField.get())
-    return 0
+def Salva_File():
+    global InputField
+    file = filedialog.asksaveasfile(defaultextension=".txt", filetypes=[("File di testo", ".txt"),
+                                                                        ("File HTML" , ".html"),
+                                                                        ("File CSS", ".css"),
+                                                                        ("Tutti i file", ".*")])
+    t = str(InputField.get("1.0", END))        
+    file.write(t)
+    file.close            
+
+def Apri_File():
+    filepath = filedialog.askopenfilename(title="Vuoi aprire un file?",filetypes=[("File di Testo", ".txt"), ("Tutti i tipi di file", ".*")])
+
+    file = open(filepath, "r")
+    read = file.read()
+    InputField.insert(END, read)
+    file.close()
+
+def Riferimento():
+    pass
+
+def Distruzione():
+    InputField.delete("1.0", END)
+
+def Generazione_numeri():
+    r = random.randint(1, 10000)
+
+    InputField.insert("1.0", str(r))
+
+
+    
+#Funzioni della SottoBar
 
 #Label
+InputField= Text(root, width=232, border=5, background=color)
 
-SaveButton = Button(root, text="Salva", command=FunzioneTest, width=6)
-FuncButton = Button(root, text="Funzioni", width=12)
-RevisionButton = Button(root, text="Revisione")
-HelpButton = Button(root, text="Help")
-InputField= Entry(root, width=520, border=5)
+#MainBAr
+
+MainBar = Menu(root)
+
+filemenu = Menu(MainBar, tearoff=0)
+
+filemenu.add_command(label="Open", command=Apri_File)
+filemenu.add_command(label="Save", command=Salva_File)
+
+ReferenceMenu = Menu(MainBar, tearoff=0)
+
+ReferenceMenu.add_command(label="Riferimento", command=Riferimento)
+
+
+FuncMenu = Menu(MainBar, tearoff=0)
+
+FuncMenu.add_command(label="Annientamento", command=Distruzione)
+FuncMenu.add_command(label="Generazione di Numeri", command=Generazione_numeri)
+
+
+MainBar.add_cascade(label="File", menu=filemenu)
+MainBar.add_cascade(label="Altro" , menu=ReferenceMenu)
+MainBar.add_cascade(label="Funzioni", menu=FuncMenu)
+
 
 #Posizioni
-InputField.grid(row=1, column=5)
+InputField.grid(row=1, column=0, ipady=256)
 
-SaveButton.grid(row=0, column=0)
-FuncButton.grid(row=0, column=1)
-RevisionButton.grid(row=0, column=2)
-HelpButton.grid(row=0, column=3)
+#MainLoop ed altro
 
-#MainLoop
+root.config(menu=MainBar)
 root.mainloop()
